81b5a2a20d384d5bae9265d692be038663460804
chandini211 <24pa5a4504@vishnu.edu.in>
2026-08-04 20:14:41 +0530

    Add files via upload

---
+++ PlacementHome.js
@@ -0,0 +1,49 @@
+<template>
+
+    <lightning-card title="Placement Dashboard" icon-name="standard:dashboard">
+
+        <div class="slds-p-around_medium">
+
+            <div class="slds-text-align_center">
+                <h1 class="slds-text-heading_large">
+                    Placement Portal
+                </h1>
+                <p class="slds-text-body_small">
+                    Welcome to the Student Placement Dashboard
+                </p>
+            </div>
+
+            <br/>
+
+            <p>
+                <strong>📅 Today's Date:</strong> {todaysDate}
+            </p>
+
+            <br/>
+
+            <div class="slds-box slds-theme_default">
+
+                <p>
+                    <strong>🏢 Number of Companies:</strong> {companies}
+                </p>
+
+                <br/>
+
+                <p>
+                    <strong>💼 Number of Jobs:</strong> {jobs}
+                </p>
+
+                <br/>
+
+                <p>
+                    <strong>📄 Applications Submitted:</strong> {applications}
+                </p>
+
+            </div>
+
+        </div>
+
+    </lightning-card>
+    <c-placement-dashboard></c-placement-dashboard>
+
+</template>
